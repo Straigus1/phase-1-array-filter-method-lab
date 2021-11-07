c86b048c7580a10f35ee5e6e7c1749deddeed047
Dayton <kirylmitch@gmail.com>
2021-11-07 16:56:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter((element) => {
+        return element.toLowerCase().includes(string.toLowerCase())
+    })
+
+}
+function fuzzyMatch(anArray, string) {
+    return anArray.filter((element) => { 
+        return element.includes(string.charAt(1))
+    })
+}
+function matchName(drivers, string)  {
+    drivers.filter(obj => obj.name === string)
+}
